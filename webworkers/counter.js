e577493a6da6271707807b1d61d9d1d2ea4c2c87
Patrick Hall <pathall@gmail.com>
2011-04-13 09:05:23 -0700

    fixed stupid html error; moved js to external file

--- webworkers/counter.js
+++ webworkers/counter.js
@@ -2,7 +2,7 @@
 function count(sequence){
   var fq = {};
 
-  for(var i=0;i<sequence.length;i++){
+  for(var i=0, max = sequence.length; i<max; i++){
     var elem = sequence[i];
 
     if ( fq[elem] === undefined ) { 
@@ -13,14 +13,13 @@ function count(sequence){
   return fq;
 }
 
-self.addEventListener('message', function(e) {
+onmessage = function(e){
 
-  var data = e.data;
-
-  var tally = count(data);
+  var data = JSON.parse(e.data),
+      tally = count(data);
 
   self.postMessage(tally);
 
-  self.close(); // Terminates the worker.
+  self.close(); 
 
-}, false);
+}
