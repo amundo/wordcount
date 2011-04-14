d0e73fa06328fc77763d178b1368b0e5714c7116
Patrick Hall <pathall@gmail.com>
2011-04-14 09:54:04 -0700

    clicking still screwy but counting the full text works

--- webworkers/longer_test_master.js
+++ webworkers/longer_test_master.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
 
+
+  function dump(obj) { return JSON.stringify(obj,null,2) } 
+  
+  function subdivide(sequence, CHUNK_MAX){
+  
+    var chunks = [], chunk = [], offset;
+  
+    for (var i = 0, max = Math.floor(sequence.length / CHUNK_MAX)+1; i < max; i++){
+      offset = i * CHUNK_MAX;
+      chunk = sequence.splice(0, CHUNK_MAX); 
+      chunks.push(chunk);    
+    }
+    return chunks;
+  }
+
+
   function render(counts){
     $('#counts tbody').empty();
     for(var word in counts){ 
       $('#counts tbody').append('<tr><td class=wordCount>'+counts[word]+'</td><td class=countedWord>' + word + '</td></tr>');
     }
   }
-
   var total = {};
 
   function calculate(){
 
-    $('.text').each(function(i){
+   var words = $.trim($('div#text').text()).split(/[\n ]+/),
+       chunks = subdivide(words, 100);
 
-      var words = $.trim( $(this).html() ).split(/[\n ]+/) ;
+console.log(chunks[chunks.length]);
 
+   $.each(chunks, function(i,chunk){
       var countingWorker = new Worker('counter.js');
 
-      countingWorker.postMessage( JSON.stringify(words) );
+      countingWorker.postMessage( JSON.stringify(chunk) );
 
       countingWorker.addEventListener('message', function(e) {
         $.each(e.data, function(word, count){
@@ -29,12 +46,12 @@ $(document).ready(function(){
         });
       }, false);
 
-    });
+    })
 
     render(total);
   }
 
-  $('#crunch').click(calculate);
+  $('#crunch').click( calculate );
 
 });
 
